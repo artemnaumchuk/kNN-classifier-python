17627961d8e14cc1e1f7eb8407d9a08bbd3a4d61
Artem Naumchuk <Artem@TMNT.Dlink>
2018-02-09 15:31:47 +0200

    backup

---
+++ knn_classifier.py
@@ -0,0 +1,15 @@
+
+class KNNClassifier(object):
+
+    def __init__(self, k=3, distance=None):
+        self.k = k
+        self.distance = distance
+
+    def fit(self, x, y):
+        pass
+
+    def predict(self, x):
+        pass
+
+    def __decision_function(self):
+        pass
